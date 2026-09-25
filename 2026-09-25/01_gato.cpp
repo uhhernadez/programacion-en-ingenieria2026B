e9b50d8d1c8a0cee0594cd7d0cde56f1520b98bf
Uriel Haile <perenganito@gmail.com>
2026-09-25 10:43:39 -0600

    Se integra el código de la última clase

---
+++ 2026-09-25/01_gato.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main () {
+    char simbolo = 92;
+    cout << simbolo << endl;
+    cout << "|" << simbolo << "___/|" << endl;
+    cout << "|\\___/|" << endl;
+    cout << "| o_o |" << endl;
+    cout << " \\_^_/" << endl;
+}
---
+++ 2026-09-25/02_calculadora.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    char operacion;
+    cout << "Ingrese operacion (+,-,*,/) :";
+    cin >> operacion;
+    if (operacion == '+') {
+        // Suma
+    } else if (operacion == '-') {
+
+    } else if (operacion == '*') {
+
+    } else if (operacion == '/') {
+
+    }
+}
